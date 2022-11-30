bd674418145fdbfeeeffa2b6091665e26dd04724
Nagashree-shetty <shettynagashree13@gmail.com>
2022-11-30 16:28:09 +0530

    provisioning container instance

--- variable.tf
+++ variable.tf
@@ -1,11 +1,12 @@
+variable "imagetag" {
+  type = "string"
+  description = "Build tag from azure yml file"
+}
+
 variable "imagebuild" {
   description = "Repository"
   default = "nagashreeshetty/newpythonwebapp:${var.imagetag}"
 }
 
-variable "imagetag"{
-  type = "string"
-  description = "Build tag from azure yml file"
-}
 
 
